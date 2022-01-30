70d96c873fe4afd8a157f769f960304f62913617
Mau Munguia <maumg1196@gmail.com>
2022-01-30 14:48:40 -0600

    Add company app's viewsets

--- apps/companies/views/companies.py
+++ apps/companies/views/companies.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""Circle views."""
 
-# Create your views here.
+# Django REST Framework
+from rest_framework import mixins, viewsets
+
+# Serializers
+from apps.companies.serializers import CompanyModelSerializer
+
+# Models
+from apps.companies.models import Company
+
+
+class CompanyViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
+    """Company view set."""
+
+    serializer_class = CompanyModelSerializer
+
+    def get_queryset(self):
+        """Get the objects we want in the request.
+        If in the future we want filters or segmentations we can do it here."""
+        queryset = Company.objects.all()
+        return queryset
